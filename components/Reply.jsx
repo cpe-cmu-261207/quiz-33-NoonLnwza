ed4acff288f3046aae0e97f5ff862f4fade09582
NoonLnwza <pubet50@gmail.com>
2022-09-15 15:11:00 +0700

    hard

--- components/Reply.jsx
+++ components/Reply.jsx
@@ -2,13 +2,12 @@ import React, { useState } from "react";
 import { comments } from "../libs/comments";
 
 export default function Reply(props) {
-  const like = comments.filter((x) => x.likeNum > 0);
   return (
     <div>
       {comments.map((x, i) => (
         <div className="d-flex gap-2 my-2 ps-5">
           <img
-            src={x.replies.username}
+            src="/profileImages/puppy.jpg"
             width="48"
             height="48"
             className="rounded-circle"
@@ -19,7 +18,7 @@ export default function Reply(props) {
             style={{ backgroundColor: "#3A3B3C" }}
           >
             <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
-              {x.replies.username}
+              หมาน้อย
             </span>
             <br />
             <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
