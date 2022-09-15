3bb290c3274c8721aab2b28b37c9c21ff3fa953c
NoonLnwza <pubet50@gmail.com>
2022-09-15 14:59:57 +0700

    so hard

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,13 +1,42 @@
 import React from "react";
+import { comments } from "../libs/comments";
 import Reply from "./Reply";
 
 export default function Comment(props) {
+  const like = comments.filter((x) => x.likeNum > 0);
+  console.log(like);
   return (
     <div>
       {/* Entire Comment div */}
+      {comments.map((x, i) => (
+        <div className="d-flex gap-2 my-2">
+          <img
+            src={x.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {x.username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{x.commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{x.likeNum} คน</span>
+            </div>
+          </div>
+        </div>
+      ))}
       <div></div>
 
       {/*render Reply here... */}
+      <Reply />
     </div>
   );
 }
--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { comments } from "../libs/comments";
 
 export default function Reply(props) {
-  return <div></div>;
+  const like = comments.filter((x) => x.likeNum > 0);
+  return (
+    <div>
+      {comments.map((x, i) => (
+        <div className="d-flex gap-2 my-2 ps-5">
+          <img
+            src={x.replies.username}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {x.replies.username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
